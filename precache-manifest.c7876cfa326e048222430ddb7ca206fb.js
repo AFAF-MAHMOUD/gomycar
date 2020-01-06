1bd3532a89b4413f42a81243908671b110ab95ec
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2020-01-06 16:18:11 +0100

    Updates

---
+++ precache-manifest.c7876cfa326e048222430ddb7ca206fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4edc2abe93876217216c45d681bb91",
+    "url": "/gomycar/index.html"
+  },
+  {
+    "revision": "879bd1ecf9731dbfecc8",
+    "url": "/gomycar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2cca58cb2d10c99681de",
+    "url": "/gomycar/static/js/2.74908ed5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gomycar/static/js/2.74908ed5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "879bd1ecf9731dbfecc8",
+    "url": "/gomycar/static/js/main.1bb0b4ce.chunk.js"
+  },
+  {
+    "revision": "19ab6d19adbf20f620f9",
+    "url": "/gomycar/static/js/runtime-main.09268f3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gomycar/static/media/logo.5d5d9eef.svg"
+  }
+]);
